4f5e455362e9e05623860fedfc33996a0f4df266
Aly <me@s5.pm>
2022-01-13 18:11:52 -0800

    Decoding that will insert U+FFFD instead of throwing an exception

--- core/shared/src/main/scala/tf/bug/utf8string/Utf8String.scala
+++ core/shared/src/main/scala/tf/bug/utf8string/Utf8String.scala
@@ -2,11 +2,13 @@ package tf.bug.utf8string
 
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
+import scala.annotation.tailrec
 
 final class Utf8String private[utf8string] (private val bytes: Array[Byte]) {
 
   def byteAt(index: Int): Byte = bytes(index)
   def getBytes(dst: ByteBuffer): Unit = dst.put(bytes)
+  def getBytes(dst: Array[Byte], offset: Int): Unit = java.lang.System.arraycopy(bytes, 0, dst, offset, bytes.length)
 
   def length: Int = bytes.length
   def size: Int = bytes.length
@@ -98,48 +100,121 @@ object Utf8String {
     new Utf8String(bytes)
   }
 
-  def parse(buf: ByteBuffer): Utf8String = {
-    var completeStringLen = buf.limit() - buf.position()
-    var idx = buf.position()
-    var done = false
-    while(!done && idx < completeStringLen) {
-      val here = buf.get(idx)
-      if((here & 0xf8) == 0xf0) {
-        if(idx + 3 < completeStringLen) {
-          idx += 4
-        } else {
-          completeStringLen = idx
-          done = true
-        }
-      } else if((here & 0xf0) == 0xe0) {
-        if(idx + 2 < completeStringLen) {
-          idx += 3
-        } else {
-          completeStringLen = idx
-          done = true
-        }
-      } else if((here & 0xe0) == 0xc0) {
-        if(idx + 1 < completeStringLen) {
-          idx += 2
-        } else {
-          completeStringLen = idx
-          done = true
-        }
-      } else if((here & 0x80) == 0x00) {
-        if(idx < completeStringLen) {
-          idx += 1
-        } else {
-          completeStringLen = idx
-          done = true
-        }
-      } else {
-        throw new IllegalStateException("Invalid UTF-8 sequence in Utf8String")
-      }
+  def apply(bytes: Array[Byte]): Utf8String = {
+    val buf = ByteBuffer.wrap(bytes)
+
+    var result = -1
+    var len = 0
+    while(len < buf.limit() && result != 0) {
+      result = validationBufferLength(buf, consume = true)
+      len += result
     }
 
-    val bytes = new Array[Byte](completeStringLen)
-    buf.get(bytes, 0, completeStringLen)
-    buf.position(idx)
+    buf.position(0)
+
+    val res = new Array[Byte](len)
+
+    result = -1
+    len = 0
+    while(len < res.length) {
+      result = validationBufferCopy(buf, res, len)
+      len += result
+    }
+
+    new Utf8String(res)
+  }
+
+  val errorReplacement: Utf8String = Utf8String("\uFFFD")
+
+  @tailrec private def continuationLenOrError(buf: ByteBuffer, ex: Int, tg: Int, consume: Boolean): Int = {
+    if(ex == 0) {
+      tg
+    } else if(buf.position() < buf.limit()) {
+      val here = buf.get()
+      if((here & 0xc0) == 0x80) continuationLenOrError(buf, ex - 1, tg, consume)
+      else errorReplacement.length
+    } else {
+      if(consume) errorReplacement.length
+      else {
+        buf.position(buf.position() - (tg - ex))
+        0
+      }
+    }
+  }
+
+  private def validationBufferLength(buf: ByteBuffer, consume: Boolean = true): Int = {
+    val here = buf.get()
+
+    if((here & 0x80) == 0x00) {
+      // 0xxxxxxx
+      1
+    } else if((here & 0xc0) == 0xc0) {
+      // 11xxxxxx
+      val seqLength = java.lang.Integer.numberOfLeadingZeros((~here) & 0x000000ff) - 24
+      continuationLenOrError(buf, seqLength - 1, seqLength, consume)
+    } else {
+      errorReplacement.length
+    }
+  }
+
+  @tailrec private def continuationSuccess(buf: ByteBuffer, ex: Int): Boolean = {
+    if(ex == 0) {
+      true
+    } else if(buf.position() < buf.limit()) {
+      val here = buf.get()
+      if((here & 0xc0) == 0x80) continuationSuccess(buf, ex - 1)
+      else false
+    } else {
+      false
+    }
+  }
+
+  private def validationBufferCopy(buf: ByteBuffer, out: Array[Byte], offset: Int): Int = {
+    val pos = buf.position()
+    val here = buf.get()
+
+    if((here & 0x80) == 0x00) {
+      // 0xxxxxxx
+      out(offset) = here
+      1
+    } else if((here & 0xc0) == 0xc0) {
+      // 11xxxxxx
+      val seqLength = java.lang.Integer.numberOfLeadingZeros((~here) & 0x000000ff) - 24
+      if(continuationSuccess(buf, seqLength - 1)) {
+        buf.position(pos)
+        buf.get(out, offset, seqLength)
+        seqLength
+      } else {
+        errorReplacement.getBytes(out, offset)
+        errorReplacement.length
+      }
+    } else {
+      errorReplacement.getBytes(out, offset)
+      errorReplacement.length
+    }
+  }
+
+  def parse(buf: ByteBuffer): Utf8String = {
+    val originalPosition = buf.position()
+
+    var result = -1
+    var len = 0
+    while(len < buf.limit() && result != 0) {
+      result = validationBufferLength(buf, consume = false)
+      len += result
+    }
+
+    buf.position(originalPosition)
+
+    val bytes = new Array[Byte](len)
+
+    result = -1
+    len = 0
+    while(len < bytes.length) {
+      result = validationBufferCopy(buf, bytes, len)
+      len += result
+    }
+
     new Utf8String(bytes)
   }
 
